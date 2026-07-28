67bed373651bd6d526b41526fbb46b4a44586e3d
Brenden <sungmin.b.park@gmail.com>
2026-07-28 11:51:26 -0700

    Implemented EarlyStopping and updated testing logic
    Implemented a class named EarlyStopping that stops model training if the validation MCC doesn't improve over a certain number of epochs. Additionally updated the testing function to return test_pred_label_list, test_logits_list, and test_label_list, which will be used for the calculation of performance metrics

--- src/utils.py
+++ src/utils.py
@@ -86,7 +86,8 @@ def test_on_testing_set(model: torch.nn.Module,
         device: Device used for testing
 
     Returns: 
-        The testing loss, mcc score, and a confusion matrix of TP, FP, FN, TN
+        The testing loss, mcc score, a confusion matrix of TP, FP, FN, TN, testing logits,
+        testing prediction labels, and the testing labels
     '''
 
     # Put model in evaluation mode
@@ -95,6 +96,9 @@ def test_on_testing_set(model: torch.nn.Module,
     # Initialize metric values
     test_loss = 0
     test_tp, test_tn, test_fp, test_fn = 0, 0, 0, 0 
+    test_pred_label_list = []
+    test_logits_list = []
+    test_label_list = []
 
     # Run the testing loop for each batch in the testing dataloader
     with torch.inference_mode():
@@ -113,6 +117,17 @@ def test_on_testing_set(model: torch.nn.Module,
             test_logit = output.logits 
             test_pred_label = test_logit.argmax(dim=1) 
 
+            # Store test logits and model prediction labels
+            for logits in test_logit:
+                test_logits_list.append(logits[1].detach().cpu().item())
+
+            for pred_label in test_pred_label:
+                test_pred_label_list.append(pred_label.detach().cpu().item())
+
+            # Store test label in list
+            for label in input['labels']:
+                test_label_list.append(label.detach().cpu().item())
+
             for idx in range(len(test_pred_label)):
                 if test_pred_label[idx] == 1 and input['labels'][idx] == 1: 
                     test_tp += 1 
@@ -123,12 +138,12 @@ def test_on_testing_set(model: torch.nn.Module,
                 elif test_pred_label[idx] == 0 and input['labels'][idx] == 1: 
                     test_fn += 1
 
-        # Calculate final validattion loss and mcc score
+        # Calculate final validation loss and mcc score
         test_mcc = calculate_mcc(test_tp, test_tn, test_fp, test_fn) 
         test_loss = test_loss / len(test_dataloader) 
         test_confusion_matrix = [test_tp, test_tn, test_fp, test_fn]
         
-    return test_loss, test_mcc, test_confusion_matrix
+    return test_loss, test_mcc, test_confusion_matrix, test_logits_list, test_pred_label_list, test_label_list
 
 
 def plot_confusion_matrix(confusion_matrix_values: list[int]):
@@ -167,3 +182,45 @@ def plot_confusion_matrix(confusion_matrix_values: list[int]):
     plt.show()
 
 
+class EarlyStopping:
+    """Early stops the training if validation MCC doesn't improve after a given patience."""
+    def __init__(self, 
+                 patience=5, 
+                 delta=0, 
+                 path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): How long to wait after last time validation MCC improved.
+                            Default: 5
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.counter = 0
+        self.best_val_mcc = None
+        self.early_stop = False
+        self.val_mcc_min = np.inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_mcc, model):
+        if self.best_val_mcc is None:
+            self.best_val_mcc = val_mcc
+            self.save_checkpoint(val_mcc, model)
+        elif val_mcc > self.best_val_mcc + self.delta:
+            # Significant improvement detected
+            self.best_val_mcc = val_mcc
+            self.save_checkpoint(val_mcc, model)
+            self.counter = 0  # Reset counter since improvement occurred
+        else:
+            # No significant improvement
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+    def save_checkpoint(self, val_mcc, model):
+        '''Saves model when validation MCC increases.'''
+        torch.save(model.state_dict(), self.path)
+        self.val_mcc_min = val_mcc
