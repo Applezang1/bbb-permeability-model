26c635f6a93755f0ccbbd747c93b147036467232
Brenden <sungmin.b.park@gmail.com>
2026-07-28 13:51:38 -0700

    Removed unnecessary functions and implemented accuracy metrics
    Removed the k-fold cross validation training loop and implemented the following accuracy metrics to args.validate: Accuracy, Precision, Recall, F1 Score, AUC-ROC, Specificity

--- train.py
+++ train.py
@@ -1,15 +1,23 @@
 import torch, yaml, argparse, optuna
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold
-from src.data_processing.dataloaders import create_dataloader, augment_dataset, tokenize_dataset
+from src.data_processing.dataloaders import create_dataloader, tokenize_dataset
 from src.data_processing.dataset_fn import space_selfies_strings
-from src.modeling.engine import train, train_step
+from src.modeling.engine import train_step, val_step
 from datasets import Dataset
 from torchinfo import summary
 from src.modeling.factory import create_model, dataset_loader
-from src.utils import save_model, load_model, plot_confusion_matrix, test_on_testing_set
+from src.utils import save_model, load_model, plot_confusion_matrix, test_on_testing_set, EarlyStopping
 from src.modeling.tune import objective
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    accuracy_score, 
+    precision_score, 
+    recall_score, 
+    f1_score, 
+    roc_auc_score,
+)
 
 
 # Define arguments to run train.py
@@ -25,9 +33,6 @@ parser.add_argument ('--validate',
 parser.add_argument('--train', 
                     action='store_true', 
                     help='Enable model training and validation')
-parser.add_argument('--full_train', 
-                    action='store_true', 
-                    help='Trains the model on 100 percent of the train_val DataFrame')
 parser.add_argument('--tune', 
                     type=int, 
                     help='Specify the number of trials for hyperparameter tuning')
@@ -38,6 +43,8 @@ args = parser.parse_args()
 with open(args.config_file, 'r') as hyp_file: 
     configs = yaml.safe_load(hyp_file)
 
+PATIENCE = configs['hyperparameters']['PATIENCE']
+VAL_SPLIT = configs['hyperparameters']['VAL_SPLIT']
 BATCH_SIZE = configs['hyperparameters']['BATCH_SIZE']
 NUM_WORKERS = configs['hyperparameters']['NUM_WORKERS']
 NUM_EPOCHS = configs['hyperparameters']['NUM_EPOCHS']
@@ -81,11 +88,11 @@ final_results = {
 
 ### Load in train_val and testing datafranes ###
 if column_name == 'SMILES':
-    train_val_dataframe = pd.read_csv('data/smiles_train_val_dataframe.csv')
-    test_dataframe = pd.read_csv('data/smiles_test_dataframe.csv')
+    train_val_dataframe = pd.read_csv('data/processed/smiles_train_dataframe.csv')
+    test_dataframe = pd.read_csv('data/processed/smiles_test_dataframe.csv')
 elif column_name == 'SELFIES': 
-    train_val_dataframe = pd.read_csv('data/selfies_train_val_dataframe.csv')
-    test_dataframe = pd.read_csv('data/selfies_test_dataframe.csv')
+    train_val_dataframe = pd.read_csv('data/processed/selfies_train_dataframe.csv')
+    test_dataframe = pd.read_csv('data/processed/selfies_test_dataframe.csv')
 
 
 ### Create testing dataloader 
@@ -114,7 +121,7 @@ test_dataloader = create_dataloader(test_dataset,
 ### Optimize the hyperparameter based on the argument
 if args.tune:
     # Maximize the val_mcc score during hyperparameter optimization
-    pruner = optuna.pruners.WilcoxonPruner(p_threshold=0.10)
+    pruner = optuna.pruners.WilcoxonPruner(p_threshold=0.15)
     study = optuna.create_study(direction="maximize", pruner=pruner)
     study.optimize(lambda trial: objective(trial,  
                                            configs, 
@@ -144,146 +151,42 @@ if args.tune:
     # Plot visualizations
     fig = optuna.visualization.plot_optimization_history(study)
     fig.show()
-    fig = optuna.visualization.plot_slice(study, params=['lr', 'weight_decay'])
+    fig = optuna.visualization.plot_slice(study, 
+                                          params=['lr', 'weight_decay', 'beta1', 'beta2', 'classifier_dropout'])
     fig.show()
 
-
+### Train the model on the train_val dataset based on argument
 if args.train:
-    ### Create training and validation dataloaders ###
-    for fold, (train_index, test_index) in enumerate(skf.split(train_val_dataframe, train_val_dataframe['labels'])):
+    # Define dictionary to store results 
+    results = {
+        'train_loss': [], 
+        'train_mcc': [],
+        'val_loss': [], 
+        'val_mcc': []
+    }
 
-        # Reinstantate model optimizer, and tokenizer 
-        model, tokenizer = create_model(configs, 
-                                        CLASSIFIER_DROPOUT)
-        model.to(device)
+    # Define early stopping object
+    early_stopping = EarlyStopping(patience=PATIENCE,
+                                   path=f'saved_models/{model_name}.pth')
 
-        optimizer = torch.optim.AdamW(params=model.parameters(), 
-                                      lr=LR, 
-                                      weight_decay=WEIGHT_DECAY, 
-                                      betas=(BETA1, BETA2))
-        
-        # Split train_val dataframe into train and validation Pandas DataFrames
-        train_dataset = train_val_dataframe.iloc[train_index, :]
-        val_dataset = train_val_dataframe.iloc[test_index, :]
+    # Split the train_val Pandas DataFrame into training and validation datasets
+    train_dataframe, val_dataframe = train_test_split(train_val_dataframe, test_size=VAL_SPLIT, random_state=42)
 
-        # Convert the train and validation Pandas DataFrames to HuggingFace datasets
-        train_dataset = Dataset.from_pandas(train_dataset)
-        val_dataset = Dataset.from_pandas(val_dataset)
+    # Convert the train Pandas DataFrames to HuggingFace training dataset
+    train_dataset = Dataset.from_pandas(train_dataframe)
 
-        # Implement required spacing to validation dataset's SELFIES strings if the model is SELFIES-TED
-        if model_name == 'SELFIES-TED':
-            # Implement spacing to validation dataset
-            val_dataset = val_dataset.to_pandas()
-            val_dataset = space_selfies_strings(val_dataset)
-            val_dataset = Dataset.from_pandas(val_dataset)
-
-        # Augment the training dataset
-        train_dataset = augment_dataset(train_dataset,  
-                                        NUM_AUGMENTATIONS, 
-                                        column_name, 
-                                        model_name)
-
-        # Tokenize the training and validation dataset               
-        train_dataset = tokenize_dataset(train_dataset,
-                                        tokenizer, 
-                                        column_name)
-        
-        val_dataset = tokenize_dataset(val_dataset, 
-                                       tokenizer, 
-                                       column_name)
-
-        # Create PyTorch training dataloader
-        train_dataloader = create_dataloader(train_dataset, 
-                                             BATCH_SIZE, 
-                                             shuffle=True,
-                                             num_workers=NUM_WORKERS)
-        
-        # Create PyTorch validation dataloader
-        val_dataloader = create_dataloader(val_dataset, 
-                                           BATCH_SIZE, 
-                                           shuffle=False, 
-                                           num_workers=NUM_WORKERS)
-
-
-        ### Training ###
-        # Train the model based on the argument
-        results = train(model=model, 
-                        fold=fold,
-                        train_dataloader=train_dataloader,
-                        val_dataloader=val_dataloader,
-                        optimizer=optimizer,
-                        device=device,
-                        num_epochs=NUM_EPOCHS)
-
-        # Store train and validation metrics
-        final_results['train_mcc'].append(results['train_mcc'])
-        final_results['train_loss'].append(results['train_loss'])
-        final_results['val_mcc'].append(results['val_mcc'])
-        final_results['val_loss'].append(results['val_loss'])
-
-
-if args.train:
-    ### Plot Results ###
-    num_epochs = np.arange(0, NUM_EPOCHS, 1)
-
-    # Instantiate variables to store average metric values across folds 
-    avg_train_mcc = []
-    avg_train_loss = []
-    avg_val_mcc = []
-    avg_val_loss = []
-
-    # Obtain the average metric values across folds
-    train_mcc = np.array(final_results['train_mcc'])
-    train_loss = np.array(final_results['train_loss'])
-    val_mcc = np.array(final_results['val_mcc'])
-    val_loss = np.array(final_results['val_loss'])
-
-    for i in range(NUM_EPOCHS):
-        avg_train_mcc.append(np.mean(train_mcc[:, i]))
-        avg_train_loss.append(np.mean(train_loss[:, i]))
-        avg_val_mcc.append(np.mean(val_mcc[:, i]))
-        avg_val_loss.append(np.mean(val_loss[:, i]))
-
-    # Plot the change in training and validation loss over epochs 
-    fig = plt.figure(figsize=(8, 8))
-    plt.plot(num_epochs, avg_train_loss, label='Train Loss')
-    plt.plot(num_epochs, avg_val_loss, label='Val Loss')
-    plt.legend()
-    plt.ylabel('Loss')
-    plt.xlabel('Number of Epochs')
-    plt.savefig('loss.png', dpi=300, bbox_inches='tight')
-    plt.show()
-
-    # Plot the change in training and validation mcc over epochs 
-    fig = plt.figure(figsize=(8, 8))
-    plt.plot(num_epochs, avg_train_mcc, label='Train MCC')
-    plt.plot(num_epochs, avg_val_mcc, label='Val MCC')
-    plt.ylabel('MCC')
-    plt.xlabel('Number of Epochs')
-    plt.legend()
-    plt.savefig('mcc.png', dpi=300, bbox_inches='tight')
-    plt.show()
-
-
-# Train the model on 100% of the train_val dataset based on argument
-if args.full_train:
-    # Convert the train_val Pandas DataFrames to HuggingFace training dataset
-    train_dataset = Dataset.from_pandas(train_val_dataframe)
-
-    # Augment the training dataset
-    train_dataset = augment_dataset(train_dataset,  
-                                    NUM_AUGMENTATIONS, 
-                                    column_name, 
-                                    model_name)
-    
-    
-    lengths = [len(tokenizer.encode(text)) for text in train_dataset['SMILES']]
-    print(np.percentile(lengths, 95))
+    # Convert the val Pandas DataFrames to HuggingFace val dataset
+    val_dataset = Dataset.from_pandas(val_dataframe)
 
     # Tokenize the training dataset               
     train_dataset = tokenize_dataset(train_dataset,
                                      tokenizer, 
                                      column_name)
+    
+    # Tokenize the validation dataset 
+    val_dataset = tokenize_dataset(val_dataset, 
+                                   tokenizer, 
+                                   column_name)
 
 
     # Create PyTorch training dataloader
@@ -292,13 +195,58 @@ if args.full_train:
                                          shuffle=True,
                                          num_workers=NUM_WORKERS)
     
+    # Create PyTorch validation dataloader 
+    val_dataloader = create_dataloader(val_dataset,
+                                       BATCH_SIZE,
+                                       shuffle=False, 
+                                       num_workers=NUM_WORKERS)
+    
     ### Full Training Loop 
     for epoch in tqdm(range(NUM_EPOCHS)):
         train_loss, train_mcc = train_step(model, 
                                            train_dataloader, 
                                            optimizer, 
                                            device)
-        tqdm.write(f' Epoch: {epoch} | Train Loss: {train_loss} | Train MCC: {train_mcc}')
+        val_loss, val_mcc = val_step(model, 
+                                     val_dataloader, 
+                                     device)
+        tqdm.write(f' Epoch: {epoch} | Train Loss: {train_loss:.3f} | Train MCC: {train_mcc:.3f} | Val Loss: {val_loss:.3f} | Val MCC: {val_mcc:.3f}')
+
+        early_stopping(val_mcc, model)
+        if early_stopping.early_stop:
+            print(" Early stopping triggered")
+            break
+
+        # Append results 
+        results['train_loss'].append(train_loss.detach().cpu().numpy())
+        results['train_mcc'].append(train_mcc)
+        results['val_loss'].append(val_loss.detach().cpu().numpy())
+        results['val_mcc'].append(val_mcc) 
+
+    ### Plot Results ###
+    num_epochs = np.arange(1, epoch+1, 1)
+
+    # Plot the change in training and validation loss over epochs 
+    fig = plt.figure(figsize=(8, 8))
+    plt.plot(num_epochs, results['train_loss'], label='Train Loss')
+    plt.plot(num_epochs, results['val_loss'], label='Val Loss')
+    plt.axvline(x=epoch-PATIENCE+1, color='red', linestyle='--', label='Early Stopping Checkpoint')
+    plt.legend()
+    plt.ylabel('Loss')
+    plt.xlabel('Number of Epochs')
+    plt.savefig('loss.png', dpi=300, bbox_inches='tight')
+    plt.show()
+
+    # Plot the change in training and validation mcc over epochs 
+    fig = plt.figure(figsize=(8, 8))
+    plt.plot(num_epochs, results['train_mcc'], label='Train MCC')
+    plt.plot(num_epochs, results['val_mcc'], label='Val MCC')
+    plt.axvline(x=epoch-PATIENCE+1, color='red', linestyle='--', label='Early Stopping Checkpoint')
+    plt.ylabel('MCC')
+    plt.xlabel('Number of Epochs')
+    plt.legend()
+    plt.savefig('mcc.png', dpi=300, bbox_inches='tight')
+    plt.show()
 
 
 # Save model weights into save_models based on argument
@@ -311,9 +259,15 @@ if args.save:
 if args.validate: 
     model_name = configs['model_information']['model_name']
     saved_model = load_model(model, f'saved_models/{model_name}.pth')
-    test_loss, test_mcc, test_confusion_matrix = test_on_testing_set(saved_model, device, test_dataloader)
+    test_loss, test_mcc, test_confusion_matrix, test_logits, test_pred_labels, test_labels = test_on_testing_set(saved_model, device, test_dataloader)
     print(f'Testing Loss: {test_loss.item():.3f}')
     print(f'Testing MCC Score: {test_mcc:.3f}')
+    print(f'Accuracy: {accuracy_score(test_labels, test_pred_labels):.3f}')
+    print(f'Precision: {precision_score(test_labels, test_pred_labels):.3f}')
+    print(f'Recall: {recall_score(test_labels, test_pred_labels):.3f}')
+    print(f'F1 Score: {f1_score(test_labels, test_pred_labels):.3f}')
+    print(f'AUC-ROC: {roc_auc_score(test_labels, test_logits):.3f}')
+    print(f'Specificity: {recall_score(test_labels, test_pred_labels, pos_label=0):.3f}')
     print(f'Total Number of Data Points in Testing Dataset: {len(test_dataset)}')
     plot_confusion_matrix(test_confusion_matrix)
 
